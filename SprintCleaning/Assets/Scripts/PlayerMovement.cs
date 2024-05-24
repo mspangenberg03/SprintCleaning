437b6a78c435187b9f300ed08f3c1244095ff1da
LorenzoJT <lorenzo.tagliaferri64@gmail.com>
2024-05-24 18:33:06 +0200

    player forward movement

--- SprintCleaning/Assets/Scripts/PlayerMovement.cs
+++ SprintCleaning/Assets/Scripts/PlayerMovement.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private float _playerSpeed = 0f;
+    [SerializeField] private Transform[] _trackPoints;
+    private Vector3 _movement = new Vector3(0, 0, 0);
+    private int _nextTrackPoint = 1;
 
     private void FixedUpdate()
     {
+        Vector3 displacement = _trackPoints[_nextTrackPoint].position - _rigidbody.position;
+        Vector3 direction = displacement.normalized;
+        _rigidbody.velocity = direction * _playerSpeed;
 
+        Vector3 nextPosition = _rigidbody.position + _rigidbody.velocity * Time.deltaTime;
+        if(Vector3.Dot(displacement, _trackPoints[_nextTrackPoint].position - nextPosition) < 0){
+            _nextTrackPoint ++;
+        }
     }
 }
