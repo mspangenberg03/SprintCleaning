2ae6296bc98936af6d31544b434859764509525c
mspangenberg03 <mspangenberg@drury.edu>
2024-05-24 17:29:51 -0500

    Work on Dirtiness system

---
+++ SprintCleaning/Assets/Scripts/DirtinessManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DirtinessManager : MonoBehaviour
+{
+    private int _currentDirtiness = 0;
+    [SerializeField,Tooltip("The amount of dirtiness")]
+    private int _maxDirtiness;
+
+    [SerializeField,Tooltip("The dirtiness bar of the player")]
+    private GameObject _healthBar;
+
+    private float _percentOfDirtiness;
+    //This function should be called from either the player script or the garbage script
+    //Param: dirtiness - The amount of dirtiness that the piece of trash adds to the player
+    public void AddDirtiness(int addedDirtiness)
+    {
+        _currentDirtiness += addedDirtiness;
+        if(_currentDirtiness >= _maxDirtiness)
+        {
+            GameOver();
+        }
+        else
+        {
+            _percentOfDirtiness = addedDirtiness/ _maxDirtiness;
+            AdjustHealthBar();
+        }
+    }
+    
+    private void GameOver()
+    {
+        Debug.Log("Game Over");
+        //SceneManager.LoadScene("GameOver");
+    }
+    private void AdjustHealthBar()
+    {
+        Transform bar = _healthBar.transform;
+        _healthBar.transform.localScale = new Vector3(bar.localScale.x - (bar.localScale.x * _percentOfDirtiness), bar.localScale.y, bar.localScale.z);
+    }
+}
