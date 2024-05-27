a2d1852d45a704f1d60d869b97838146bbe88678
mspangenberg03 <mspangenberg@drury.edu>
2024-05-26 21:20:02 -0500

    Added TextMeshPro

--- SprintCleaning/Assets/Scripts/Garbage.cs
+++ SprintCleaning/Assets/Scripts/Garbage.cs
@@ -4,22 +4,9 @@ using UnityEngine;
 
 public class Garbage : MonoBehaviour
 {
-    [SerializeField,Tooltip("The type of tool that is required to collect the garbage")]
-    private GameObject _toolRequired;
 
     [SerializeField,Tooltip("How much dirtiness does this garbage add")]
     private int _dirtiness = 10;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     //TODO: Check if the player has the right tools
     private void OnTriggerEnter(Collider other)
     {
