79f8e68e0bdaae6b574bc60021a9bf85453dd7ca
LorenzoJT <lorenzo.tagliaferri64@gmail.com>
2024-05-29 00:18:23 +0200

    Updated UITollBar

--- SprintCleaning/Assets/Scripts/UI/ToolBar.cs
+++ SprintCleaning/Assets/Scripts/UI/ToolBar.cs
@@ -6,18 +6,18 @@ using UnityEngine.UI;
 public class ToolBar : MonoBehaviour
 {
     private PlayerToolManager PlayerToolManager => GameObject.FindObjectOfType<PlayerToolManager>();
-    private List<ToolBase> ActiveTools => PlayerToolManager.GetComponent<List<ToolBase>>();
-    [SerializeField] private ToolUI[] _availableTools;
+    private List<ToolBase> ActiveTools => PlayerToolManager._toolList;
     private Image[] _toolSprites => gameObject.GetComponentsInChildren<Image>();
-
+    
     public void DrawSpritesOnToolAdd()
     {
+        
         for (int i = 0; i < ActiveTools.Count; i++)
         {
-            if (ActiveTools[i] != null) { }
+            if (ActiveTools[i] != null)
                 _toolSprites[i + 1].overrideSprite = ActiveTools[i]._toolUI.Sprite;
         }
-        if (ActiveTools.Count > 1)
-            ActiveTools.Reverse();
+
+
     }
 }
