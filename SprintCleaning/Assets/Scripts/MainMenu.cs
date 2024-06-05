8078e7ec8a0c3289d7e677e9b95ee8412412cc19
mspangenberg03 <mspangenberg@drury.edu>
2024-06-05 16:15:09 -0500

    Work on Settings menu

--- SprintCleaning/Assets/Scripts/MainMenu.cs
+++ SprintCleaning/Assets/Scripts/MainMenu.cs
@@ -2,20 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _mainMenuPanel;
+    [SerializeField]
+    private GameObject _settingsMenuPanel;
+
+    [SerializeField]
+    private Slider _audioSlider;
+
+    private bool _exitingGame = false;
+
+    
+    private void Start()
+    {
+        _audioSlider.value = PlayerPrefs.GetFloat("AudioSettings");
+        Camera.main.GetComponent<AudioSource>().volume = _audioSlider.value;
+        
+    }
     public void StartButton()
     {
         SceneManager.LoadScene("Main");
     }
     public void ExitGame()
     {
+        _exitingGame=true;
         #if UNITY_STANDALONE_WIN
             Application.Quit();
         #endif
         #if UNITY_EDITOR
             UnityEditor.EditorApplication.isPlaying = false;
         #endif
+    }
+    public void Settings()
+    {
+        _mainMenuPanel.SetActive(false);
+        _settingsMenuPanel.SetActive(true);
+    }
+    public void ReturnToMenu()
+    {
+        _mainMenuPanel.SetActive(true);
+        _settingsMenuPanel.SetActive(false);
+    }
+
+    public void ChangeAudio()
+    {
+        if (!_exitingGame)
+        {
+            Camera.main.GetComponent<AudioSource>().volume = _audioSlider.value;
+            PlayerPrefs.SetFloat("AudioSettings", _audioSlider.value);
+        }
     }
 }
