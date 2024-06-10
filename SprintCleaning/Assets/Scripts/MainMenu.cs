77d1952a57c05d9d587093837192972b4228e605
mspangenberg03 <mspangenberg@drury.edu>
2024-06-10 13:40:12 -0500

    Added a settings menu

--- SprintCleaning/Assets/Scripts/MainMenu.cs
+++ SprintCleaning/Assets/Scripts/MainMenu.cs
@@ -1,8 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
 
 public class MainMenu : MonoBehaviour
 {
@@ -14,13 +16,24 @@ public class MainMenu : MonoBehaviour
     [SerializeField]
     private Slider _audioSlider;
 
-    private bool _exitingGame = false;
+    [SerializeField]
+    private Toggle _fullscreenToggle;
 
-    
+    [SerializeField]
+    private TextMeshProUGUI _resolutionDropdownText;
+
+    private TMP_Dropdown _dropdown;
+
+    private int index;
     private void Start()
     {
         _audioSlider.value = PlayerPrefs.GetFloat("AudioSettings");
         Camera.main.GetComponent<AudioSource>().volume = _audioSlider.value;
+        _fullscreenToggle.isOn = Screen.fullScreen;
+        _dropdown = _resolutionDropdownText.transform.parent.GetComponent<TMP_Dropdown>();
+        index = PlayerPrefs.GetInt("Index",0);
+        _dropdown.value = index;
+        _resolutionDropdownText.text =_dropdown.options[index].text;
         
     }
     public void StartButton()
@@ -29,7 +42,6 @@ public class MainMenu : MonoBehaviour
     }
     public void ExitGame()
     {
-        _exitingGame=true;
         #if UNITY_STANDALONE_WIN
             Application.Quit();
         #endif
@@ -50,10 +62,25 @@ public class MainMenu : MonoBehaviour
 
     public void ChangeAudio()
     {
-        if (!_exitingGame)
-        {
-            Camera.main.GetComponent<AudioSource>().volume = _audioSlider.value;
-            PlayerPrefs.SetFloat("AudioSettings", _audioSlider.value);
-        }
+        
+        Camera.main.GetComponent<AudioSource>().volume = _audioSlider.value;
+        PlayerPrefs.SetFloat("AudioSettings", _audioSlider.value);
+        
+    }
+    public void SetFullscreen()
+    {
+        Screen.fullScreen = _fullscreenToggle.isOn;
+        
+    }
+    public void ChangeResolution()
+    {
+        string[] str= _resolutionDropdownText.text.Split("x");
+        int width = int.Parse(str[0]);
+        int height = int.Parse(str[1]);
+        PlayerPrefs.SetInt("Width", width);
+        PlayerPrefs.SetInt("Height", height);
+        Screen.SetResolution(width, height,_fullscreenToggle.isOn);
+        index = _dropdown.value;
+        PlayerPrefs.SetInt("Index", index);
     }
 }
