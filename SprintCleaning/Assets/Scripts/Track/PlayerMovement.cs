f336fdec4d260b009edb771c37a743de5e861a6c
Casey Walsh-Warder <cwalshwarder@brandeis.edu>
2024-06-13 20:09:11 -0400

    Synchronize music with player position
    Changes to main scene are only making the audio sources for music not play on awake and apply prefab overrides on main camera. I think.

--- SprintCleaning/Assets/Scripts/Track/PlayerMovement.cs
+++ SprintCleaning/Assets/Scripts/Track/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+[DefaultExecutionOrder(-10)]
 public class PlayerMovement : MonoBehaviour
 {
     private const int TARGET_POINT_INDEX = 1;
@@ -14,7 +15,6 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _speedText;
 
     private float _speedMultiplier = 1f;
-    private float _lastGarbageSlowdownTime = float.NegativeInfinity;
     private float _currentTargetLane;
     private bool _changingLanes;
     
@@ -38,7 +38,6 @@ public class PlayerMovement : MonoBehaviour
 
     private float _jumpPosition;
     private float _jumpSpeed;
-
 
     private float CurrentForwardsSpeed
     {
@@ -75,6 +74,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Start()
     {
+        
         _positionOnMidline = TrackGenerator.Instance.TrackPieces[0].EndTransform.position + Vector3.up * _settings.PlayerVerticalOffset;
         _rigidbody.position = _positionOnMidline;
         _rigidbody.transform.position = _positionOnMidline;
@@ -100,33 +100,25 @@ public class PlayerMovement : MonoBehaviour
     {
         _speedMultiplier *= playerSpeedMultiplier;
         _speedMultiplier = Mathf.Max(_settings.MinForwardsSpeed / _settings.BaseForwardsSpeed, _speedMultiplier);
-        _lastGarbageSlowdownTime = Time.time;
     }
-
-
 
     private void FixedUpdate()
     {
+        if (AudioSettings.dspTime < GameplayMusic.Instance.AudioStartTime)
+        {
+            return;
+        }
+
         PollInputsOncePerFrame();
         DevHelper.Instance.GameplayReproducer.StartNextFixedUpdate();
         DevHelper.Instance.GameplayReproducer.SaveOrLoadMovementInputs(ref _leftInput, ref _rightInput, ref _leftInputDown, ref _rightInputDown, ref _jumpInput);
-        
-/*
-        if (!DevHelper.Instance.TrashCollectionTimingInfo.CheckTrashCollectionConsistentIntervals)
-        {
-            // accelerate forwards
-            if (Time.time > _lastGarbageSlowdownTime + _settings.AccelerationPauseAfterGarbageSlowdown)
-            {
-                if (_speedMultiplier >= 1f)
-                    CurrentForwardsSpeed += _settings.ForwardsAcceleration * Time.deltaTime;
-                else
-                    CurrentForwardsSpeed += _settings.ForwardsAccelerationWhileBelowBaseSpeed * Time.deltaTime;
-            }
-        }
-        */
 
         TrackPiece trackPiece = TrackGenerator.Instance.TrackPieces[TARGET_POINT_INDEX];
         float t = trackPiece.FindTForClosestPointOnMidline(_positionOnMidline);
+
+        //double time = ((double)GameplayMusic.Instance._musicSources[0].timeSamples) / GameplayMusic.Instance._musicSources[0].clip.frequency;
+        //Debug.Log(t + " " + time);
+
 
         Vector3 midlineVelocity = TrackMidlineVelocity(trackPiece, t);
 
