a778196ffe9f269f5878711c8acfbaaed572f2c0
Casey Walsh-Warder <cwalshwarder@brandeis.edu>
2024-06-20 12:43:26 -0400

    Polish for playtest

---
+++ SprintCleaning/Assets/Scripts/Garbage/GarbagePickupAnimationTrigger.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarbagePickupAnimationTrigger : MonoBehaviour
+{
+    [SerializeField] private Garbage _garbage;
+
+    private int _enterCount;
+
+    public void Reset()
+    {
+        _enterCount = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _enterCount++;
+            if (_enterCount != 1)
+                return;
+            GameObject player = other.transform.parent.gameObject;
+            Animator animator = player.GetComponentInChildren<Animator>();
+            if (!_garbage.Obstacle)
+                animator.SetTrigger("PickUp");
+        }
+    }
+
+    public void CheckTriggerAnimation(Collider other)
+    {
+        if (_enterCount != 0)
+            return;
+        GameObject player = other.transform.parent.gameObject;
+        Animator animator = player.GetComponentInChildren<Animator>();
+        if (!_garbage.Obstacle)
+            animator.SetTrigger("PickUp");
+    }
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Player"))
+    //        _enterCount--;
+    //}
+}
