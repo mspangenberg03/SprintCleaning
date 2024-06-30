2ef2675c1390d495fb8f9fa1004612e980567d6b
Casey Walsh-Warder <cwalshwarder@brandeis.edu>
2024-06-29 23:49:57 -0400

    Fade music out when lose
    Otherwise the audio kinda keeps playing for like 1/10th of a second when the volume starts for the next run

--- SprintCleaning/Assets/Scripts/Audio/FadeMixerGroup.cs
+++ SprintCleaning/Assets/Scripts/Audio/FadeMixerGroup.cs
@@ -34,10 +34,4 @@ public static class FadeMixerGroup {
         }
         yield break;
     }
-
-    public static void SetVolume(AudioMixer audioMixer, string exposedParam, float newVolume)
-    {
-        newVolume = Mathf.Clamp(newVolume, 0.0001f, 1);
-        audioMixer.SetFloat(exposedParam, Mathf.Log10(newVolume) * 20);
-    }
 }
